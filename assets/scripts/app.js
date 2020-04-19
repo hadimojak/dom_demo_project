6b1a1230135815908663b12033df1ecaf0422d46
Hadi Arbabi <h.arbabi@banicm.com>
2020-04-20 02:04:02 +0430

    userInputsAdded

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,8 @@ const startAddMovie = document.querySelector("header button");
 
 const backDrop = document.getElementById("backdrop");
 const addModalCancelButton = addMovieModal.querySelector(".btn--passive");
+const addModalAddButton = addModalCancelButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
 
 const toggleBackDrop = () => {
     backDrop.classList.toggle("visible");
@@ -22,7 +24,29 @@ const backDropClickHandler = () => {
 const cancelAddMovie = () => {
     toggleMovieModal();
 };
+const addMovieHandler = () => {
+    // const x = addMovieModal.querySelector("#title").value;
+    // const y = addMovieModal.querySelector("#image-url").value; //just for test
+    // const z = addMovieModal.firstElementChild.lastElementChild.value;
+    // console.log(`${x}` + `${y}` + z);
+    const title = userInputs[0].value;
+    const image = userInputs[1].value;
+    const rating = userInputs[2].value;
+    const ul = document.getElementById("movie-list");
+    if (
+        title.trim() === "" ||
+        image.trim() === "" ||
+        rating.trim() === "" ||
+        +rating < 1 ||
+        +rating > 5
+    ) {
+        alert("bad input!");
+        return;
+    }
+    toggleMovieModal();
+};
 
 startAddMovie.addEventListener("click", toggleMovieModal);
 backDrop.addEventListener("click", backDropClickHandler);
 addModalCancelButton.addEventListener("click", cancelAddMovie);
+addModalAddButton.addEventListener("click", addMovieHandler);
